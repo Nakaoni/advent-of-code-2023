66de610f25630eec2b94310812b5764fa364c396
Nakaoni <cho_tenro@hotmail.fr>
2023-12-02 19:06:33 +0100

    day one: part_one

---
+++ day_one/day_one.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/Nakaoni/advent-of-code-2023/day_one/part_one"
+	"github.com/Nakaoni/advent-of-code-2023/day_one/utils"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("day_one: ")
+	log.SetFlags(0)
+
+	lines, err := utils.GetFileContent("./day_one/assets/input.txt")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	resultOne, errOne := part_one.GetResult(lines)
+	//resultTwo, errTwo := part_two.GetResult(lines)
+
+	utils.PrintResult("part_one", resultOne, errOne)
+	//utils.PrintResult("part_two", resultTwo, errTwo)
+}
---
+++ day_one/go.mod
@@ -0,0 +1,3 @@
+module github.com/Nakaoni/advent-of-code-2023/day_one
+
+go 1.21.4
